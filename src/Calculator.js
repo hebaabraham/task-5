21a0d070455eb988e7c45419805e392d350fa066
hebaabraham <22bcad72@kristujayanti.com>
2024-07-27 00:03:41 +0900

    fasd

---
+++ src/Calculator.js
@@ -0,0 +1,41 @@
+// src/Calculator.js
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } catch {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'C'].map((button) => (
+          <button key={button} onClick={() => handleButtonClick(button)}>
+            {button}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
